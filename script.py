045ad6868afdcf89b4cb2e030737efe6074d5588
legion-nodes <legion.validator.cosmos@gmail.com>
2024-08-14 15:47:52 -0400

    Initial setup with GitHub Actions

---
+++ script.py
@@ -0,0 +1,9 @@
+import json
+from datetime import datetime
+
+def main():
+    now = datetime.now()
+    print(f"Hello from GitHub Actions! The current time is {now}")
+
+if __name__ == "__main__":
+    main()
